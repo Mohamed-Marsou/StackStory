7289be874b4c1fb2d0c968e793c7983915e25752
el bekkari yassine <100382885+elbekkari-yassine@users.noreply.github.com>
2023-09-06 04:03:02 +0200

    Update Contact.vue

--- Blog/js/src/views/blogViews/Contact.vue
+++ Blog/js/src/views/blogViews/Contact.vue
@@ -1,16 +1,39 @@
 <!--! Always use  ""<script setup>"" COMPOSITION API SYNTAX ALWAYS -->
-<script setup>
 
-</script>
 <template>
-    <div class="contact-main">
-        <h1>Contact us !!</h1>
+    
+    
+    <div class="container">
+        <h1>Contact Us</h1>
+        <p>Hello! Do you have any questions or suggestions about this site, or just want to say Hi? Send me a message using below form. I will get back to you as soon as possible.</p>
+        <form action="#" >
+            <div class="form-group">
+                
+                <input type="text" id="fullname" name="fullname" placeholder="Full name"  required>
+            </div>
+            <div class="form-group">
+                
+                <input type="email" id="email" name="email"  placeholder="Email" required>
+            </div>
+            <div class="form-group">
+                
+                <input type="text" id="subject" name="subject" placeholder="Subject" required>
+            </div>
+            <div class="form-group">
+                
+                <textarea id="message" name="message" placeholder="Message" required></textarea>
+            </div>
+            <button type="submit">send Message</button>
+        </form>
     </div>
-</template>
-
-<style lang="scss">
-    .contact-main{
-  width: 100%;
-  min-height: 100vh;
-}
+      
+  </template>
+  
+  <script  setup>
+    
+  </script>
+  <style lang="scss" scoped>
+  @import '../../styles/contact.scss';
+  @import '../../styles/_variables.scss';
 </style>
+  
