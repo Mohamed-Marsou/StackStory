3b95b81c080925f50f5b92189dbe13a6f2964cc3
zaydinani <zaydinani@protonmail.com>
2023-09-25 20:37:20 +0100

    database
    added database migration

--- Blog/back-end/database/migrations/2023_09_25_183613_admins.php
+++ Blog/back-end/database/migrations/2023_09_25_183613_admins.php
@@ -11,13 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('admins', function (Blueprint $table) {
             $table->id();
             $table->string('first_name');
             $table->string('last_name');
             $table->string('email')->unique();
             $table->string('password');
-            $table->boolean('is_guest');
+            $table->string('image_url')->nullable();
             $table->timestamp('email_created_at');
             $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
@@ -29,6 +29,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('admins');
     }
 };
---
+++ Blog/back-end/database/migrations/2023_09_25_184357_articles.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('articles', function (Blueprint $table) {
+            $table->id();
+            $table->string('article_title');
+            $table->text('article_summary');
+            $table->longText('article_content');
+            $table->foreignId('author_id')->constrained('admins');
+            $table->timestamps();
+            $table->timestamp('published_at')->nullable();
+            $table->string('status')->default('pending');
+            $table->string('meta_title');
+            $table->string('slug');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('articles');
+    }
+};
